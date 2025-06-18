f5d4d2b04745d5a5f4b191c1ccc14441b842b113
Raghav Verma <90718322+rv-raghav@users.noreply.github.com>
2025-06-18 11:41:44 +0000

    python basics completed

---
+++ 01_basics/hello.py
@@ -0,0 +1,7 @@
+from hello_python import chai
+chai("ginger tea")
+# This code imports the `chai` function from the `hello_python` module
+# and calls it with the argument "ginger tea".
+# The output will be:
+# ginger tea
+# Output: ginger tea
