aeec6af4812393d006f29ba5354cbb6f73b1fa39
Raghav Verma <90718322+rv-raghav@users.noreply.github.com>
2025-07-28 08:36:52 +0000

    python-learn-in-public

---
+++ python_project/main.py
@@ -0,0 +1,3 @@
+# This is my first python program (Comments -> #)
+print("Hey!!")
+print("Hey, How are you ?")
---
+++ python_project/variables.py
@@ -0,0 +1,22 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# Strings
+first_name = "Raghav"
+food = "pizza"
+email = "bro123@fake.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is {email}")
+
+# Integers
+age = 22
+quantity = 3
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You are buying {quantity} items")
+print(f"Your class has {num_of_students} students")
+
+# Float
